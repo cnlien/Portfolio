e617d3050f92acff125b87b54584f226d097d72c
Christopher Lien <cnlien@gmail.com>
2019-10-02 16:35:07 -0400

    committing changes to web projects component

--- src/components/webProjects.js
+++ src/components/webProjects.js
@@ -5,59 +5,53 @@ import './projects.css';
 const WebProjects = (props) => {
     return (
     <div>
-    <Col>
-        <Row className="sectionHeader">
-            <h1>Web</h1>
-        </Row>
+        <Col>
+            <Row className="sectionHeader">
+                <h1>Web</h1>
+            </Row>
 
-        <Row classNam="webProjects">
-            <Card className="projectCard">
-                <CardImg className ="" src="./img/flw_mockups.jpg" />
-                <CardBody>
-                    <CardTitle><h1>French Lick Winery</h1></CardTitle>
-                    <CardSubtitle><p>HTML/CSS, JavaScript, SquareSpace</p></CardSubtitle>
-                    <Button>About The Project</Button>
-                    <Button>View The Site</Button>
-                </CardBody>
-            </Card>
-            <Card className="projectCard">
-                <CardImg className ="" src="./img/flw_mockups.jpg" />
-                <CardBody>
-                    <CardTitle><h1>Spirits of French Lick</h1></CardTitle>
-                    <CardSubtitle><p>HTML/CSS, JavaScript, SquareSpace</p></CardSubtitle>
-                    <Button>About The Project</Button>
-                    <Button>View The Site</Button>
-                </CardBody>
-            </Card>
-            <Card className="projectCard">
-                <CardImg className ="" src="./img/flw_mockups.jpg" />
-                <CardBody>
-                    <CardTitle><h1>Replate</h1></CardTitle>
-                    <CardSubtitle><p>HTML/CSS, JavaScript, SquareSpace</p></CardSubtitle>
-                    <Button>About The Project</Button>
-                    <Button>View The Site</Button>
-                </CardBody>
-            </Card>
-            <Card className="projectCard">
-                <CardImg className ="" src="./img/flw_mockups.jpg" />
-                <CardBody>
-                    <CardTitle><h1>French Lick Winery</h1></CardTitle>
-                    <CardSubtitle><p>HTML/CSS, JavaScript, SquareSpace</p></CardSubtitle>
-                    <Button>About The Project</Button>
-                    <Button>View The Site</Button>
-                </CardBody>
-            </Card>
-            <Card className="projectCard">
-                <CardImg className ="" src="./img/flw_mockups.jpg" />
-                <CardBody>
-                    <CardTitle><h1>French Lick Winery</h1></CardTitle>
-                    <CardSubtitle><p>HTML/CSS, JavaScript, SquareSpace</p></CardSubtitle>
-                    <Button>About The Project</Button>
-                    <Button>View The Site</Button>
-                </CardBody>
-            </Card>
-        </Row>
-    </Col>
+            <Row classNam="webProjects">
+
+                {/*French Lick Winery Card Start*/}
+                <Card className="projectCard">
+                    <CardImg src="./img/flw_mockups.jpg" />
+                    <CardBody>
+                        <CardTitle><h1>French Lick Winery</h1></CardTitle>
+                        <CardSubtitle><p>HTML/CSS, JavaScript, SquareSpace</p></CardSubtitle>
+                        <Button>About The Project</Button>
+                        <Button>View The Site</Button>
+                    </CardBody>
+                </Card>
+                {/*French Lick Winery Card End*/}
+
+                {/*Spirits of French Lick Card Start*/}
+                <Card className="projectCard">
+                    <CardImg className ="" src="./img/sfl-mockups.jpg" />
+                    <CardBody>
+                       <CardTitle><h1>Spirits of French Lick</h1></CardTitle>
+                       <CardSubtitle><p>HTML/CSS, JavaScript, SquareSpace</p></CardSubtitle>
+                       <Button>About The Project</Button>
+                       <Button>View The Site</Button>
+                    </CardBody>
+                </Card>
+                {/*Spirits of French Lick Card End*/}
+
+                {/*Replate Card Start*/}
+                <Card className="projectCard">
+                    <CardImg className ="" src="./img/iphoneMockUp.png" />
+                    <CardBody>
+                       <CardTitle><h1>Replate App</h1></CardTitle>
+                       <CardSubtitle><p>HTML/CSS, Less, UI/UX</p></CardSubtitle>
+                       <div className="projectButton">
+                        <Button>About The Project</Button>
+                        <Button>View The Site</Button>
+                       </div>
+                    </CardBody>
+                </Card>
+                {/*Replate Card End*/}
+
+            </Row>
+        </Col>
     </div>
     );
 
