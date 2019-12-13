b452cf96563ff89ab247c3b5bc8692dad0d0e67f
Christopher Lien <cnlien@gmail.com>
2019-12-13 16:49:18 -0500

    removed working portfolio to new branch and added a landing page

--- src/App.js
+++ src/App.js
@@ -12,18 +12,20 @@ function App() {
         <h1 className="heroName">Chris Lien</h1>
         <h2 className="heroLocation">Indianapolis, IN, USA</h2>
         <h3 className="heroSkill">Front End Web Developer | Graphic Designer</h3>
-
+        <br/>
+        <p>While I work on this portfolio, connect with me.</p>
         <div className="socialLinksContainer">
           <a href="http://www.github.com/cnlien"><img className='socialLinks githubLogo' src='./img/github.svg' alt='Git Hub Logo'/></a>
           <a href="http://www.linkedin.com/in/cnlien"><img className='socialLinks linkedinLogo' src='./img/linkedin.svg' alt='LinkedIn Logo'/></a>
         </div>
 
       </div>
-  
+
+      {/*   
       <div className="container">
         <WebProjects />
         <DesignProjects/>
-      </div>
+      </div> */}
     </>
 
   );
