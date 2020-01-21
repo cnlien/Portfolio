70d23f35c2da991bf412ff01a1d0ca92920cd89a
Christopher Lien <cnlien@gmail.com>
2020-01-21 10:12:38 -0500

    removed react-image-gallery.scss from index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
-import 'react-image-gallery/styles/scss/image-gallery.scss';
+
 import { BrowserRouter as Router } from 'react-router-dom'
 
 ReactDOM.render(
